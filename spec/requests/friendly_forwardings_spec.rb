0192f198b880eacc26853aa370b04d22cc3c0d1e
Ian Cole <icole@teliax.com>
2012-01-27 13:26:29 -0700

    Working on user tests

--- spec/requests/friendly_forwardings_spec.rb
+++ spec/requests/friendly_forwardings_spec.rb
@@ -1,9 +1,12 @@
 require 'spec_helper'
 
 describe "FriendlyForwardings" do
-  describe "GET /friendly_forwardings" do
-    it "works! (now write some real specs)" do
-      get friendly_forwardings_path
-    end
-  end
+  
+  it "should forward to the requested page after signin" do
+    user = Factory(:user)
+    visit edit_user_path(user)
+    fill_in :email,     :with => user.email
+    fill_in :password,  :with => user.password
+    click_button
+    respone.should render_template('user/edit')
 end
