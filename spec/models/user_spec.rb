de7820eb77f3f801aa6648a22b54a98828624bee
Ian Cole <icole@teliax.com>
2012-01-27 18:13:57 -0700

    Almost finshed with user implementation

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -132,5 +132,25 @@ describe User do
       end
     end
   end
+  
+  describe "admin attribute" do
+    
+    before(:each) do
+      @user = User.create!(@attr)
+    end
+    
+    it "should response to admin" do
+      @user.should respond_to(:admin)
+    end
+    
+    it "should not be an admin by default" do
+      @User.should_not be_admin
+    end
+    
+    it "should be convertible to be admin" do
+      @user.toggle!(:admin)
+      @user.should be_admin
+    end
+  end
 end
 
